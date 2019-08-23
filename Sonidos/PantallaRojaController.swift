6d309334533e78cb980df355c3d41f78b33530b0
Alumno <frn-a>
2019-08-23 14:23:53 -0700

    cambiar pantalla

---
+++ Sonidos/PantallaRojaController.swift
@@ -0,0 +1,18 @@
+//
+//  PantallaRojaController.swift
+//  Sonidos
+//
+//  Created by Alumno on 8/23/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PantallaRojaController : UIViewController{
+    @IBAction func doTapRegresar(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
--- Sonidos/ViewController.swift
+++ Sonidos/ViewController.swift
@@ -12,8 +12,23 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var player : AVAudioPlayer?
+    var playerMusicaFondo : AVAudioPlayer?
+    
+    var reproduccion = false
     
     @IBAction func doTapReproducir(_ sender: Any) {
+        if reproduccion {
+            let pathMusicaFondo = Bundle.main.path(forResource: "AUDIO29.wav", ofType: nil)
+            let urlMusicaFondo = URL(fileURLWithPath: pathMusicaFondo!)
+            
+            do{
+                player = try AVAudioPlayer(contentsOf: urlMusicaFondo)
+            }catch{
+                
+            }
+        } else{
+            reproduccion = true
+        }
         if player != nil{
             player?.play()
         }
@@ -24,8 +39,12 @@ class ViewController: UIViewController {
         let path = Bundle.main.path(forResource: "b.wav", ofType: nil)
         let url = URL(fileURLWithPath: path!)
         
+        
+        
         do{
             player = try AVAudioPlayer(contentsOf: url)
+            
+            //playerMusicaFondo?.play()
         }catch{
             
         }
